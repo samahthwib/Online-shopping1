f4e9cea49ed92c440c7cbbad73d5b37392a2a4fd
samahthwib <samah.thweib@hotmail.com>
2020-02-05 22:28:54 +0200

    add

--- js/file1.js
+++ js/file1.js
@@ -1,6 +1,6 @@
 
 
-var Age= prompt("what is your baby age?")
+var Age= prompt("what is your baby age?");
 console .log(Age);
 var section;
 if(Age>=3 && Age<12){
@@ -19,6 +19,80 @@ document.write('<h3>'+section+'</h3>');
 
 
 
+/////////////////////////////////
+var brand = prompt("Please enter your favorite brand", "zara kids");
+   document.write("Enjoy your shopping in " + brand + " "+"brand section");
+   if (brand != null) {
+     document.getElementById("demo").innerHTML =
+     "Enjoy your shopping in " + brand + " "+"brand section";
+   }
+
+
+   ///////////////////////////////
+   var person = prompt("Please enter your baby name", "ahmad");
+   document.write("Hello " + person + "  "+"mommy ! How are you today?");
+   if (person != null) {
+     document.getElementById("demo1").innerHTML =
+     "Hello " + person + "mommy ! How are you today?";
+   }
+ 
+
+
+//////////////////////////////////////
+
+
+ var babyGender= prompt("what is your baby gender?");
+console .log(babyGender);
+var section1;
+if(babyGender == 'Girle'){
+    section1='Girle section';
+
+}
+else if(babyGender == 'Boy'){
+    section1='Boy section';
+}
+
+else{
+    section1='Both section';
+
+}
+document.write('<h3>'+section1+'</h3>');
+//////////////////////////////////////////////////////////
+//function myFunction() {
+      
+   
+
+   
+
+
+////////////////////////////////////////////////
+//function order(Age){
+
+  //var Age= prompt("what do you want to order? clothes or ho")
+//function addAge(Age){
+
+//console .log(Age);
+//var section;
+//if(Age>=3 && Age<12){
+ //   section='Children section';
+
+//}
+//else if(Age>=0 && Age<3){
+  //  section='newborn section';
+//}
+
+//else{
+  //  section='Not available';
+
+//}
+//return section;
+//}
+//var Age= prompt("what is your baby age?")  // we can here change the name of var ex: age5
+//document.write('<h3>'+addAge(Age)+'</h3>'); // we should but the same name , age5 , we can call the function in HTML also
+
+
+
+
 function myFunction() {
    // var person = prompt("www.Online-shopping.com wants to");
     var txt;
@@ -34,8 +108,8 @@ function myFunction() {
 
   //function myFunction() {
       
-    var person = prompt("Please enter your baby name", "ahmad");
-    document.write("Hello " + person + "  "+"mommy ! How are you today?");
+   // var person = prompt("Please enter your baby name", "ahmad");
+    //document.write("Hello " + person + "  "+"mommy ! How are you today?");
     //if (person != null) {
       //document.getElementById("demo").innerHTML =
       //"Hello " + person + "mommy ! How are you today?";
